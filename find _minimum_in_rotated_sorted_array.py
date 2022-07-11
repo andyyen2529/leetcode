4d77fececeb977d86a3f90e500a52bd7f920eead
Andy <andyyen2529@gmail.com>
2022-07-11 14:13:24 +0800

    find_minimum

---
+++ find _minimum_in_rotated_sorted_array.py
@@ -0,0 +1,11 @@
+from typing import List 
+from collections import defaultdict
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # [1,2,3,4,5] how many rotation times to become [3,4,5,1,2]
+        
+
+a = Solution()
+print(a.findMin([3,4,5,1,2])
